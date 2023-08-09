a367b4cbb997478a0e39096da98f0f20ed25a168
Vo Phong <phongwind21092001@gmail.com>
2023-08-09 09:17:32 +0700

    feat(phong): responsive for homepage level large tablet

--- src/views/Navigation/Navigation.js
+++ src/views/Navigation/Navigation.js
@@ -11,99 +11,99 @@ class Navigation extends React.Component {
     render() {
         return (
             <div className="nav-container">
-                <NavLink to="/" exact>
+                <NavLink to="/" exact className="nav-responsive1-show">
                     <img src={icons.home} className="nav-icon"></img>
-                    Trang chủ
+                    <p className="nav-child">Trang chủ</p>
                 </NavLink>
-                <NavLink to="/shorts">
+                <NavLink to="/shorts" className="nav-responsive1-show">
                     <img src={icons.shorts} className="nav-icon"></img>
-                    Shorts
+                    <p className="nav-child">Shorts</p>
                 </NavLink>
-                <NavLink to="/subscriptions">
+                <NavLink to="/subscriptions" className="nav-responsive1-show">
                     <img src={icons.subscibe} className="nav-icon"></img>
-                    Kênh đăng ký
+                    <p className="nav-child">Kênh đăng ký</p>
                 </NavLink>
-                <hr></hr>
-                <NavLink to="/library">
+                <hr className="nav-responsive1-hide"></hr>
+                <NavLink to="/library" className="nav-responsive1-show">
                     <img src={icons.library} className="nav-icon"></img>
-                    Thư viện
+                    <p className="nav-child">Thư viện</p>
                 </NavLink>
-                <NavLink to="/history">
+                <NavLink to="/history" className="nav-responsive1-hide">
                     <img src={icons.recent} className="nav-icon"></img>
-                    Video đã xem
+                    <p className="nav-child">Video đã xem</p>
                 </NavLink>
-                <NavLink to="/myvideo">
+                <NavLink to="/myvideo" className="nav-responsive1-hide">
                     <img src={icons.channel} className="nav-icon"></img>
-                    Video của bạn
+                    <p className="nav-child">Video của bạn</p>
                 </NavLink>
-                <NavLink to="/playlist">
+                <NavLink to="/playlist" className="nav-responsive1-hide">
                     <img src={icons.bookmark} className="nav-icon"></img>
-                    Xem sau
+                    <p className="nav-child">Xem sau</p>
                 </NavLink>
-                <NavLink to="/favorite">
+                <NavLink to="/favorite" className="nav-responsive1-hide">
                     <img src={icons.fvr} className="nav-icon"></img>
-                    Video đã thích
+                    <p className="nav-child">Video đã thích</p>
                 </NavLink>
-                <NavLink to="/more">
+                <NavLink to="/more" className="nav-responsive1-hide">
                     <img src={icons.down} className="nav-icon"></img>
-                    Thêm
+                    <p className="nav-child">Thêm</p>
                 </NavLink>
-                <hr></hr>
-                <p className="nav-title">Khám phá</p>
-                <NavLink to="/trending">
+                <hr className="nav-responsive1-hide"></hr>
+                <p className="nav-title nav-responsive1-hide" >Khám phá</p>
+                <NavLink to="/trending" className="nav-responsive1-hide">
                     <img src={icons.trending} className="nav-icon"></img>
-                    Thịnh hành
+                    <p className="nav-child">Thịnh hành</p>
                 </NavLink>
-                <NavLink to="/music">
+                <NavLink to="/music" className="nav-responsive1-hide">
                     <img src={icons.music} className="nav-icon"></img>
-                    Âm nhạc
+                    <p className="nav-child">Âm nhạc</p>
                 </NavLink>
-                <NavLink to="/game">
+                <NavLink to="/game" className="nav-responsive1-hide">
                     <img src={icons.gaming} className="nav-icon"></img>
-                    Trò chơi
+                    <p className="nav-child">Trò chơi</p>
                 </NavLink>
-                <NavLink to="/news">
+                <NavLink to="/news" className="nav-responsive1-hide">
                     <img src={icons.news} className="nav-icon"></img>
-                    Tin tức
+                    <p className="nav-child">Tin tức</p>
                 </NavLink>
-                <NavLink to="/sport">
+                <NavLink to="/sport" className="nav-responsive1-hide">
                     <img src={icons.sports} className="nav-icon"></img>
-                    Thể thao
+                    <p className="nav-child">Thể thao</p>
                 </NavLink>
-                <hr></hr>
-                <p className="nav-title">Dịch vụ khác của Youtube</p>
-                <NavLink to="/yPremium">
+                <hr className="nav-responsive1-hide"></hr>
+                <p className="nav-title nav-responsive1-hide">Dịch vụ khác của Youtube</p>
+                <NavLink to="/yPremium" className="nav-responsive1-hide">
                     <img src={pictures.premium} className="nav-picture"></img>
-                    YouTube Premium
+                    <p className="nav-child">YouTube Premium</p>
                 </NavLink>
-                <NavLink to="/yStudio">
+                <NavLink to="/yStudio" className="nav-responsive1-hide">
                     <img src={pictures.studio} className="nav-picture"></img>
-                    YouTube Studio
+                    <p className="nav-child">YouTube Studio</p>
                 </NavLink>
-                <NavLink to="/yMusic">
+                <NavLink to="/yMusic" className="nav-responsive1-hide">
                     <img src={pictures.music} className="nav-picture"></img>
-                    YouTube Music
+                    <p className="nav-child">YouTube Music</p>
                 </NavLink>
-                <NavLink to="/yKids">
+                <NavLink to="/yKids" className="nav-responsive1-hide">
                     <img src={pictures.kid} className="nav-picture"></img>
-                    YouTube Kids
+                    <p className="nav-child">YouTube Kids</p>
                 </NavLink>
-                <hr></hr>
-                <NavLink to="/setting">
+                <hr className="nav-responsive1-hide"></hr>
+                <NavLink to="/setting" className="nav-responsive1-hide">
                     <img src={icons.setting} className="nav-icon"></img>
-                    Cài đặt
+                    <p className="nav-child">Cài đặt</p>
                 </NavLink>
-                <NavLink to="/reporthistory">
+                <NavLink to="/reporthistory" className="nav-responsive1-hide">
                     <img src={icons.report} className="nav-icon"></img>
-                    Nhật ký báo cáo
+                    <p className="nav-child">Nhật ký báo cáo</p>
                 </NavLink>
-                <NavLink to="/help">
+                <NavLink to="/help" className="nav-responsive1-hide">
                     <img src={icons.help} className="nav-icon"></img>
-                    Trợ giúp
+                    <p className="nav-child">Trợ giúp</p>
                 </NavLink>
-                <NavLink to="/feedback">
+                <NavLink to="/feedback" className="nav-responsive1-hide">
                     <img src={icons.res} className="nav-icon"></img>
-                    Gửi ý kiến phản hồi
+                    <p className="nav-child">Gửi ý kiến phản hồi</p>
                 </NavLink>
             </div>
         )
--- src/views/Subscriptions/ContentSub/ContentSub.js
+++ src/views/Subscriptions/ContentSub/ContentSub.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navigation from "../../Navigation/Navigation";
 import './ContentSub.scss'
 import VideoCard from "../../VideoCard/VideoCard";
+import images from "../../../assests/hardcode/images";
 
 class ContentSub extends React.Component {
     render() {
@@ -9,7 +10,12 @@ class ContentSub extends React.Component {
             <div className="contentSub-container">
                 <Navigation />
                 <div className="contentSub-content">
-                    <VideoCard />
+                    <VideoCard
+                        thumbnail={images.mv7}
+                        avtChanel={images.mv7a}
+                        nameVideo={'HOÀ MINZY x BÙI CÔNG NAM | TẾT BÙNG NỘI LỰC | OFFICIAL MV'}
+                        nameChanel={'Hòa Minzy'}
+                        views={'1.000.000 lượt xem'} />
                 </div>
             </div>
         )
