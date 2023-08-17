7c1b608878076cf0135edbdf76f632d10f3a9d2c
Vo Phong <phongwind21092001@gmail.com>
2023-08-17 16:33:17 +0700

    test 2 for deploy

--- src/views/Subscriptions/Subscriptions.js
+++ src/views/Subscriptions/Subscriptions.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../Header/Header";
 import ContentSub from "./ContentSub/ContentSub";
+import Header from '../Header/Header'
 
 class Subscriptions extends React.Component {
     render() {
